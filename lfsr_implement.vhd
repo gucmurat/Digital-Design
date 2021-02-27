16dbb3373f845562cc6e958d6b471f6184a6bcb8
gucmurat <79659434+gucmurat@users.noreply.github.com>
2021-02-27 12:24:24 +0300

    Add files via upload

---
+++ lfsr_implement.vhd
@@ -0,0 +1,77 @@
+-------------------------------------------------------
+-- Design Name : Pi Approximation by random 8-bit LFSR
+-- File Name   : lfsr_implement.vhd
+-- Coder       : Murat Güç
+-------------------------------------------------------
+library ieee;
+   use IEEE.NUMERIC_STD.ALL;
+	use IEEE.STD_LOGIC_1164.ALL;
+
+use ieee.math_real.uniform;
+use ieee.math_real.floor;
+use ieee.std_logic_unsigned.all;
+use work.lfsr_pkg.all;                 --import LFSR package
+
+entity lfsr_implement is
+    port (
+        clk    :in  std_logic;
+        rst    :in  std_logic;
+        outC   :out std_logic_vector(15 downto 0);  --out vector for counter
+        lfsr1  :out std_logic_vector(7 downto 0);   --first random 8-bit vector for output to see in sim
+		  lfsr2  :out std_logic_vector(7 downto 0);   --second random 8-bit vector for output to see in sim
+		  pi     :out std_logic_vector(31 downto 0)   --last output-main purpose
+    );
+end entity;
+
+architecture rtl of lfsr_implement is
+    constant taps   :std_logic_vector (7 downto 0) := "10001110";
+    signal   lfsr_a :std_logic_vector (7 downto 0);
+    signal   lfsr_b :std_logic_vector (7 downto 0);
+	 signal outC_reg : unsigned (15 downto 0);                       --intermediate vector - carrier
+	 signal   pi_reg : unsigned (31 downto 0);                       --intermediate vector - carrier
+	 signal    cons1 : unsigned (15 downto 0) := "0000000000000100"; --4 in 10's system to calculate ratio of areas which are square and quarter circle
+	 signal    cons2 : unsigned (15 downto 0) := "0000000000000101"; --5 in 10's system to calculate pi within 5000 trial/dart 
+begin
+		
+    process (clk, rst) 
+		variable counter : INTEGER range 0 to 10000000;
+		variable flag : STD_LOGIC := '0';
+		variable A : unsigned (15 downto 0);                        --variables needed for calculation
+		variable B : unsigned (15 downto 0);
+		variable X : integer:=0;
+		variable Y : integer:=0;
+		variable D   : unsigned(15 DOWNTO 0) := "1111111000000001"; --the max boundary point 255*255 acc. to max random 8-bit multiplication
+	 begin
+        
+		  if (rst = '1') then
+           lfsr_a <=  (others=>'0');
+           lfsr_b <=  (others=>'0');
+			  counter:=0;
+        elsif (rising_edge(clk)) then
+				if(flag = '0') then
+					if(counter<10000) then
+						lfsr_a <= many_to_one_fb (lfsr_a, taps);  --generating number from LFSR package by functions
+						lfsr_b <= one_to_many_fb (lfsr_b, taps);  
+						X := to_integer(unsigned(lfsr_a));
+						A := (to_unsigned(integer(X),16));
+						Y := to_integer(unsigned(lfsr_b));
+						B := (to_unsigned(integer(Y),16));
+						if ((A*A)+(B*B) <= (D)) then               --checking whether (x,y) point is in circle or not
+							counter := counter + 1;                 --then increment the counter	
+						elsif(counter=10000) then          
+							flag:='1';                              --flag is for stopping and starting the clk loop
+					
+						end if;
+					end if;
+				end if;
+        end if;
+	 outC_reg<= (to_unsigned(integer(counter),16));           --counter gets out from process
+    end process;
+	 
+	 outC <= std_logic_vector(outC_reg);
+	 lfsr1<= lfsr_a;                                          --assiging intermediate signal to output
+	 lfsr2<= lfsr_b;
+	 pi_reg <= outC_reg * cons1 / cons2;                      --calculation by unsigned vectors (3626*4/5=3060)"pi*1000
+	 pi <= std_logic_vector(pi_reg);
+	 
+end architecture;
---
+++ lfsr_tb1.vhd
@@ -0,0 +1,84 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
+-- Create Date:   01:15:29 01/09/2021
+-- Design Name:   
+-- Module Name:   D:/Others/projectCode1/lfsr_tb1.vhd
+-- Project Name:  projectCode1
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: lfsr_implement
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+library ieee;
+    use ieee.std_logic_1164.all;
+    use ieee.std_logic_unsigned.all;
+    use ieee.std_logic_textio.all;
+    use std.textio.all;
+
+entity lfsr_tb1 is
+end entity;
+architecture test of lfsr_tb1 is
+
+    signal clk    :std_logic := '0';
+    signal rst    :std_logic := '1';
+    signal outC   :std_logic_vector(15 downto 0);
+	 signal lfsr1  :std_logic_vector(7 downto 0);
+	 signal lfsr2  :std_logic_vector(7 downto 0);
+	 signal pi     :std_logic_vector(31 downto 0);
+	 
+    component lfsr_implement is
+    port (
+        clk    :in  std_logic;
+        rst    :in  std_logic;
+        outC   :out std_logic_vector(15 downto 0);
+		  lfsr1  :out std_logic_vector(7 downto 0);
+		  lfsr2  :out std_logic_vector(7 downto 0);
+		  pi     :out std_logic_vector(31 downto 0)
+    );
+    end component;
+begin
+
+    -- Generate clock
+    clk <= not clk after 0.1 ns;
+    rst <= '0' after 0 ns;
+
+    Inst_lfsr : lfsr_implement
+    port map (
+        clk    => clk,   
+        rst    => rst,
+        outC => outC,
+		  lfsr1 => lfsr1,
+		  lfsr2 => lfsr2,
+		  pi => pi
+    );
+
+    -- Display the time and result
+    process (clk)
+        variable wrbuf :line;
+    begin
+        if (clk = '1') then
+            write(wrbuf, string'("Time: ")); write(wrbuf, now);
+            write(wrbuf, string'("; lfsr_1: ")); write(wrbuf, conv_integer(lfsr1)); --print are of random numbers and the counter
+				write(wrbuf, string'("; lfsr_2: ")); write(wrbuf, conv_integer(lfsr2));
+				write(wrbuf, string'("; counter: ")); write(wrbuf, conv_integer(outC));
+            writeline(output, wrbuf);
+        end if;
+    end process;
+
+end architecture;
